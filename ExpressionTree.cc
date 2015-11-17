d5005cddbd1775b060f35bb3a274b4e7f2edba9b
P.R. van der Waal <s1559451@leidenuniv.nl>
2015-11-17 15:35:22 +0100

    worked on translate function

--- ExpressionTree.cc
+++ ExpressionTree.cc
@@ -1,4 +1,10 @@
 #include "ExpressionTree.h" 
+#include <sstream>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
 
 bool ExpressionTree::isBinaryOperator(TreeNode<Token> *p) const{
   if (p->getInfo().type == Token::VARIABLE){
@@ -8,17 +14,54 @@ bool ExpressionTree::isBinaryOperator(TreeNode<Token> *p) const{
   return false;
 }
 
+
 void ExpressionTree::parse(string s){
-  stringstream stream;
-  string ins;
-  s >> ins;
-  
-  
-  
-  cout << ins << endl;
+  istringstream ss(s);
+  Token a;
+  while (ss >> s){
+    a = translate(s);
+    insert(a);
+  } 
 }
 
+Token ExpressionTree::translate(string s){
+  switch (atoi(s)) {
+   //case (s.front() >= '0' && s.front() <= '9'):
+     //return Token a {Token::NUMBER, atof(s.c_str())};
+   case atoi("pi"):
+     return Token a {Token::NUMBER, 3.14};
+ 
+   case atoi("+"):
+     return Token a {Token::PLUS, s.front()};
 
+   case atoi("-"):
+     return Token a {Token::MINUS, s.front()};
+
+   case atoi("*"):
+     return Token a {Token::MULTIPLY, s.front()};
+
+   case atoi("/"):
+     return Token a {Token::DIVIDE, s.front()};
+
+   case atoi("^"):
+     return Token a {Token::POWER, s.front()};
+   
+   case atoi("cos"):
+     return Token a {Token::COS, 'c'};
+   
+   case atoi("sin"):
+     return Token a {Token::SIN, 's'};
+
+   //case (s.front() >= 'a' && s.front <= 'z')
+     //return Token a {Token::VARIABLE, s.front()};
+
+   default:
+     return Token a {Token::ERROR, '!'};
+  
+  }
+
+
+}
 
 
 
--- ExpressionTree.h
+++ ExpressionTree.h
@@ -1,10 +1,11 @@
 #ifndef _ExpressionTree_H_
 #define _ExpressionTree_H_
+
 #include "tree.h" 
 
 struct Token {
   enum {
-    PLUS, MINUS, NUMBER, VARIABLE, COS, SIN, DIVIDE, MULTIPLY, POWER
+    PLUS, MINUS, NUMBER, VARIABLE, COS, SIN, DIVIDE, MULTIPLY, POWER, ERROR
   } type;
   union {
     char variable;
@@ -17,7 +18,8 @@ class ExpressionTree : public Tree<Token>{
   public:
     void parse (string s);
   private:
-    bool isBinaryOperator(TreeNode<Token> *p) const; 
+    bool isBinaryOperator(TreeNode<Token> *p) const;
+    Token translate (string s); 
 
 
 };
